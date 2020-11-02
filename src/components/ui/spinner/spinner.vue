9c6b1ce60ae164eeceb89b4044058fc527e65184
Constantine Rafikov <kon.rafikov@yandex.ru>
2020-11-03 02:47:08 +0300

    Spinner component

---
+++ src/components/ui/spinner/spinner.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="spinner">
+    <div class="spinner__loader"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Spinner',
+  }
+</script>
+<style lang="scss" scoped>
+  @import "spinner";
+</style>
+
